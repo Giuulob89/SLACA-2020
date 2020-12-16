1915469e49ab576691c9abd4994558e2b29a6435
Giuulob89 <giulialobo89@gmail.com>
2020-12-16 01:09:36 -0300

    Comenting all the code and doing the readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# SLACA-2019
+# SLACA-2019 - Aplicação Referente à candidatura de estágio de Dev Front-End na Galoá(Chuva Inc.)
+
+## Nesta aplicação, utilizei as linguagens HTML5, CSS3 e JavaScript.
+
+* HTML5: implementei todo o conteúdo da página, seguindo fielmente o protótipo passado;
+* CSS3: utilizei a ferramenta para estilizar todos os elementos da página e para colocar todos em seus devidos lugares, da maneira mais responsiva possível;
+* JavaScript: com ele, criei funções para realizar as ações que estavam presentes no protótipo navegável fornecido, foquei nas funções que estavam lá e acabei não criando outras funções em outros elementos, apesar de serem funções com uma certa facilidade de implementação.
+
+### Obs: Não utilizei a ferramenta bootstrap por não ver necessidade. Creio que poderia otimizar o tempo em algumas atividades, contudo, consegui entregar a atividade no prazo de 7 dias, sem precisar dele.
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+/*---------------Função Ver Mais---------------*/
+
 function Vermais() {
     var pontos=document.getElementById("pontos");
     var maisTexto=document.getElementById("mais");
@@ -14,6 +16,48 @@ function Vermais() {
         btnVermais.style.display="none";
     }
 }
+
+/*---------------Função Criar Tópico---------------*/
+
+function CriarTopico() {
+    var introducaoantes = document.getElementById("introducao-antes");
+    var introducaodepois = document.getElementById("introducao-depois");
+    var btnCriarTopico = document.getElementById("btn-criar-topico");
+
+    if (introducaoantes.style.display === "none") {
+        introducaoantes.style.display = "inline";
+        introducaodepois.style.display = "none";
+        btnCriarTopico.innerHTML = "criar tópico";
+    }
+    else {
+        introducaoantes.style.display = "none";
+        introducaodepois.style.display = "inline";
+        btnCriarTopico.style.display = "none";
+    }
+}
+
+/*---------------Função Criar Novo Tópico---------------*/
+
+function CriarNovoTopico() {
+    var introducaoaposenvio = document.getElementById("introducao-apos-envio");
+    var introducaodepois = document.getElementById("introducao-depois");
+    var btnCriarTopico2 = document.getElementById("btn-criar-topico2");
+    var btnEnviar = document.getElementById("btn-enviar");
+
+    if (introducaoaposenvio.style.display === "none") {
+        introducaoaposenvio.style.display = "inline";
+        introducaodepois.style.display = "none";
+        btnCriarTopico2.innerHTML = "criar tópico";
+    }
+    else {
+        introducaoaposenvio.style.display = "none";
+        introducaodepois.style.display = "inline";
+        btnCriarTopico2.style.display = "none";
+        btnEnviar.style.display="inline"
+    }
+}
+
+/*---------------Função Enviar---------------*/
 
 function Enviar() {
     var introducaodepois = document.getElementById("introducao-depois");
@@ -34,6 +78,8 @@ function Enviar() {
     }
 }
 
+/*---------------Função Expandir Card---------------*/
+
 function ExpandirCard() {
     var card21 = document.getElementById("card2-1");
     var card22 = document.getElementById("card2-2");
@@ -49,6 +95,8 @@ function ExpandirCard() {
         card22.style.display = "inline"
     }
 }
+
+/*---------------Função Retrair Card---------------*/
 
 function RetrairCard() {
     var card21 = document.getElementById("card2-1");
